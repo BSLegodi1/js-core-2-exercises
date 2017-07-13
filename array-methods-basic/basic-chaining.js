17b9aeeb6b070a9e8e0ddda07436829e2df5892d
BSLegodi1 <bslegodi@gmail.com>
2017-07-13 20:32:13 +0100

    Refactered getUpperCaseVowels() to use .map .filter

--- array-methods-basic/basic-chaining.js
+++ array-methods-basic/basic-chaining.js
@@ -1,16 +1,30 @@
 /*
     Refactor this to use .map and .filter together (chaining)
 */
-var vowels = ["a", "e", "i", "o", "u"];
+// var vowels = ["a", "e", "i", "o", "u"];
 
-function getUpperCaseVowels(letters) {
-    var result = [];
-    for(var i = 0; i < letters.length; i++) {
-        if(vowels.indexOf(letters[i]) !== -1) {
-            result.push(letters[i].toUpperCase());
-        }
-    }
+// function getUpperCaseVowels(letters) {
+//     var result = [];
+//     for(var i = 0; i < letters.length; i++) {
+//         if(vowels.indexOf(letters[i]) !== -1) {
+//             result.push(letters[i].toUpperCase());
+//         }
+//     }
+//     return result;
+// };
+
+var vowels = ["a", "e", "i", "o", "u"];
+function getUpperCaseVowels(alphabet) {
+    var result = alphabet.filter(letter => vowels.indexOf(letter)>-1).map(letter=>letter.toUpperCase());
+  
+
     return result;
-};
+}
+
+
+
+
+
+
 
 module.exports = getUpperCaseVowels;
